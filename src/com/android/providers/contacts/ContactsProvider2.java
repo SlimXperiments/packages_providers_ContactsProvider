LineageOS/android_packages_providers_ContactsProvider

Fix IllegalArgumentException.
Include alternative display name in the phone lookup projection, since
the BT app needs it. Fixes this backtrace:

D BluetoothPbapObexServer: search by number, requestSize=1 offset=0 searchValue=4436029097
E DatabaseUtils: Writing exception to parcel
E DatabaseUtils: java.lang.IllegalArgumentException: Invalid column display_name_alt
E DatabaseUtils: 	at android.database.sqlite.SQLiteQueryBuilder.computeProjection(SQLiteQueryBuilder.java:632)
E DatabaseUtils: 	at android.database.sqlite.SQLiteQueryBuilder.buildQuery(SQLiteQueryBuilder.java:447)
E DatabaseUtils: 	at android.database.sqlite.SQLiteQueryBuilder.query(SQLiteQueryBuilder.java:393)
E DatabaseUtils: 	at com.android.providers.contacts.ContactsProvider2.query(ContactsProvider2.java:6066)
E DatabaseUtils: 	at com.android.providers.contacts.ContactsProvider2.queryLocal(ContactsProvider2.java:5846)
E DatabaseUtils: 	at com.android.providers.contacts.ContactsProvider2.queryInternal(ContactsProvider2.java:4810)
E DatabaseUtils: 	at com.android.providers.contacts.ContactsProvider2.query(ContactsProvider2.java:4773)
E DatabaseUtils: 	at android.content.ContentProvider$Transport.query(ContentProvider.java:189)
E DatabaseUtils: 	at android.content.ContentProviderNative.onTransact(ContentProviderNative.java:113)
E DatabaseUtils: 	at android.os.Binder.execTransact(Binder.java:351)
E DatabaseUtils: 	at dalvik.system.NativeStart.run(Native Method)

Change-Id: I15f531c5b0ea4b0a4a9de864872dd3256667e568
JIRA:CYAN-2204

src/com/android/providers/contacts/ContactsProvider2.java
@@ -845,6 +845,7 @@ public class ContactsProvider2 extends AbstractContactsProvider
             .add(PhoneLookup._ID, "contacts_view." + Contacts._ID)
             .add(PhoneLookup.LOOKUP_KEY, "contacts_view." + Contacts.LOOKUP_KEY)
             .add(PhoneLookup.DISPLAY_NAME, "contacts_view." + Contacts.DISPLAY_NAME)
+            .add(PhoneLookup.DISPLAY_NAME_ALTERNATIVE, "contacts_view." + Contacts.DISPLAY_NAME_ALTERNATIVE)
             .add(PhoneLookup.LAST_TIME_CONTACTED, "contacts_view." + Contacts.LAST_TIME_CONTACTED)
             .add(PhoneLookup.TIMES_CONTACTED, "contacts_view." + Contacts.TIMES_CONTACTED)
             .add(PhoneLookup.STARRED, "contacts_view." + Contacts.STARRED)

SlimXperiments/packages_providers_ContactsProvider

src/com/android/providers/contacts/ContactsProvider2.java
@@ -889,6 +889,7 @@ public class ContactsProvider2 extends AbstractContactsProvider
             .add(PhoneLookup._ID, "contacts_view." + Contacts._ID)
             .add(PhoneLookup.LOOKUP_KEY, "contacts_view." + Contacts.LOOKUP_KEY)
             .add(PhoneLookup.DISPLAY_NAME, "contacts_view." + Contacts.DISPLAY_NAME)
+            .add(Contacts.DISPLAY_NAME_ALTERNATIVE, "contacts_view." + Contacts.DISPLAY_NAME_ALTERNATIVE)
             .add(PhoneLookup.LAST_TIME_CONTACTED, "contacts_view." + Contacts.LAST_TIME_CONTACTED)
             .add(PhoneLookup.TIMES_CONTACTED, "contacts_view." + Contacts.TIMES_CONTACTED)
             .add(PhoneLookup.STARRED, "contacts_view." + Contacts.STARRED)